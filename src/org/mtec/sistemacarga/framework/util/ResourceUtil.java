6819519257d0eba643abb66431a13f8951bbc707
macielbombonato <macielbombonato@e08b796b-7f9d-4906-dee1-7c4ab3280acd>
2010-11-27 19:53:35 +0000

    Novas Funcionalidades: + Arquivo de configuracao convertido para objeto serializado. Esta funcionalidade possibilita que um arquivo de configuracao (texto) seja serializado em um objeto e distribuido, assim o responsavel pela execucao da carga nao tem acesso as senhas utilizadas. + Valor default para os parametros de entrada. Esta funcionalidade possibilita que quando em uma classe de carga os atributos mapeados com a anotacao Property sejam inicializados no contexto de classe com um valor default, este aparecera na tela no ato em que a aplicacao eh executada, com isso, podemos sugerir valores padroes ao executor da aplicacao. + Ajustes nos tipos de dados. Foi incluido o tipo de dado Timestamp. + Pequenos ajustes no mecanismo de execucao de threads. + Inclusao de Libs para novos skins para a aplicacao, porem, ainda nao estao em uso.

--- src/org/mtec/sistemacarga/framework/util/ResourceUtil.java
+++ src/org/mtec/sistemacarga/framework/util/ResourceUtil.java
@@ -5,9 +5,13 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+
+import org.mtec.sistemacarga.framework.vo.ConfigurationVO;
 
 /**
  * <strong><h1>Definição</h1></strong>
@@ -25,6 +29,7 @@ public class ResourceUtil {
 	/**
 	 * Collection para representar repositório de objetos properties
 	 */
+	@SuppressWarnings("unchecked")
 	private static Map mapProperties;
 	
 	/**
@@ -38,16 +43,138 @@ public class ResourceUtil {
 	private File f;
 	
 	/**
+	 * Ponteiro que direciona para o arquivo de propriedades criptografado
+	 */
+	private File fDat;
+	
+	/**
+	 * Objeto que representa o arquivo de configuracoes.
+	 */
+	private ConfigurationVO conf;
+	
+	/**
+	 * Variável de controle para cerificacao se o arquivo de propriedades criptografado ja foi salvo.
+	 */
+	private boolean hasSafeSave = false;
+	
+	/**
 	 * É necessário que o construtor default seja privado para
 	 * a implementação do pattern de Singleton
-	 *
 	 */
+	@SuppressWarnings("unchecked")
 	private ResourceUtil() {
 		mapProperties = new HashMap();
+		
+		if (!hasSafeSave) {
+			hasSafeSave = true;
+			if (f == null) {
+				f = new File(ResourceLocation.RESOURCES_PATH + ResourceLocation.CONF_PROPERTIES + ".properties");	
+			}
+			if (fDat == null) {
+				fDat = new File(ResourceLocation.RESOURCES_PATH + ResourceLocation.CONF_PROPERTIES + ".properties.dat");	
+			}
+			
+			if (!fDat.exists()) {
+				propertySafeSave();
+			} else {
+				loadSafeSaveConfig();
+			}
+		}
 	}
 	
 	public static ResourceUtil getInstance() {
 		return instance;
+	}
+	
+	private void propertySafeSave() {
+		if (f.exists()) {
+			try {
+				if (!fDat.exists()) {
+					ConfigurationVO conf = setConfiguracoesVO();
+					
+					ObjectOutputStream out = new ObjectOutputStream(
+							new FileOutputStream(fDat));
+					out.writeObject(conf);
+					out.flush();
+					out.close();
+				}
+			} catch (IOException e) {
+				Log.error("Ocorreu um erro de escrita no arquivo [" + f.getAbsolutePath() + ".properties]!");
+				Log.error( e );
+			}
+		}
+	}
+
+	private ConfigurationVO setConfiguracoesVO() {
+		ConfigurationVO conf = new ConfigurationVO();
+		conf.setFullPackageName(new StringBuffer(this.getProperty("resources.filter.full_package_name")));
+		conf.setJarFilesDir(new StringBuffer(this.getProperty("resources.file.jar_file")));
+		
+		conf.setLogXmlAppenderPath(new StringBuffer(this.getProperty("log.xmlAppender.path")));
+		conf.setLogFileAppenderPath(new StringBuffer(this.getProperty("log.fileAppender.path")));
+		conf.setLogRefreshTime(new StringBuffer(this.getProperty("log.refreshTime")));
+
+		conf.setDataBaseTnsNamesDataBasePath(new StringBuffer(this.getProperty("resources.database.tnsnames_database.path")));
+		conf.setDataBaseConfigPoolSize(new StringBuffer(this.getProperty("resources.database.config.poolsize")));
+		conf.setDataBaseConfigMaxPoolSize(new StringBuffer(this.getProperty("resources.database.config.maxpoolsize")));
+		conf.setDataBaseCommitPoint(new StringBuffer(this.getProperty("resources.database.commit_point")));
+		
+		conf.setDataBaseDriverOrigem(new StringBuffer(this.getProperty("resources.database.driver.origem")));
+		conf.setDataBaseUrlOrigem(new StringBuffer(this.getProperty("resources.database.url.origem")));
+		conf.setDataBaseUserOrigem(new StringBuffer(this.getProperty("resources.database.user.origem")));
+		conf.setDataBasePasswordOrigem(new StringBuffer(this.getProperty("resources.database.pass.origem")));
+		
+		conf.setDataBaseDriverDestino(new StringBuffer(this.getProperty("resources.database.driver.destino")));
+		conf.setDataBaseUrlDestino(new StringBuffer(this.getProperty("resources.database.url.destino")));
+		conf.setDataBaseUserDestino(new StringBuffer(this.getProperty("resources.database.user.destino")));
+		conf.setDataBasePasswordDestino(new StringBuffer(this.getProperty("resources.database.pass.destino")));
+		return conf;
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void loadSafeSaveConfig() {
+		if (fDat.exists()) {
+			try {
+				ObjectInputStream in = new ObjectInputStream(new FileInputStream(fDat));
+				conf = (ConfigurationVO) in.readObject();
+				in.close();
+				Properties prop = getConfiguracoesVO(conf);
+				mapProperties.put(ResourceLocation.CONF_PROPERTIES, prop);
+			} catch (FileNotFoundException e) {
+				Log.error("Arquivos de propriedades criptografado nao encontrado.");
+				Log.error(e);
+			} catch (IOException e) {
+				Log.error("Falha ao abrir o arquivo de propriedades criptografado.");
+				Log.error(e);
+			} catch (ClassNotFoundException e) {
+				Log.error("Objeto de configuracao nao encontrado.");
+				Log.error(e);
+			}
+		}
+	}
+
+	private Properties getConfiguracoesVO(ConfigurationVO conf) {
+		Properties prop = new Properties();
+		
+		prop.setProperty("resources.filter.full_package_name", conf.getFullPackageName().toString());
+		prop.setProperty("resources.file.jar_file", conf.getJarFilesDir().toString());
+		prop.setProperty("log.xmlAppender.path", conf.getLogXmlAppenderPath().toString());
+		prop.setProperty("log.fileAppender.path", conf.getLogFileAppenderPath().toString());
+		prop.setProperty("log.refreshTime", conf.getLogRefreshTime().toString());
+		prop.setProperty("resources.database.tnsnames_database.path", conf.getDataBaseTnsNamesDataBasePath().toString());
+		prop.setProperty("resources.database.config.poolsize", conf.getDataBaseConfigPoolSize().toString());
+		prop.setProperty("resources.database.config.maxpoolsize", conf.getDataBaseConfigMaxPoolSize().toString());
+		prop.setProperty("resources.database.commit_point", conf.getDataBaseCommitPoint().toString());
+		prop.setProperty("resources.database.driver.origem", conf.getDataBaseDriverOrigem().toString());
+		prop.setProperty("resources.database.url.origem", conf.getDataBaseUrlOrigem().toString());
+		prop.setProperty("resources.database.user.origem", conf.getDataBaseUserOrigem().toString());
+		prop.setProperty("resources.database.pass.origem", conf.getDataBasePasswordOrigem().toString());
+		prop.setProperty("resources.database.driver.destino", conf.getDataBaseDriverDestino().toString());
+		prop.setProperty("resources.database.url.destino", conf.getDataBaseUrlDestino().toString());
+		prop.setProperty("resources.database.user.destino", conf.getDataBaseUserDestino().toString());
+		prop.setProperty("resources.database.pass.destino", conf.getDataBasePasswordDestino().toString());
+		
+		return prop;
 	}
 	
 	/**
@@ -60,32 +187,26 @@ public class ResourceUtil {
 	 * @return value
 	 */
 	@SuppressWarnings("unchecked")
-	public String getProperty(String arquivoProperty, String key) {
+	public String getProperty(String key) {
 		String retorno = null;
-		//
-		f = new File(ResourceLocation.RESOURCES_PATH + arquivoProperty.concat(".properties"));
-		//
-		if (mapProperties.containsKey(arquivoProperty) ) {
-			retorno = ((Properties)mapProperties.get(arquivoProperty)).getProperty(key);
+		if (mapProperties.containsKey(ResourceLocation.CONF_PROPERTIES) ) {
+			retorno = ((Properties)mapProperties.get(ResourceLocation.CONF_PROPERTIES)).getProperty(key);
 		} else {
 			try {
 				Properties prop = new Properties();
-				Log.info("Processando a carga do arquivo [" + arquivoProperty + ".properties] em getProperty()...");
-				//
+				Log.info("Processando a carga do arquivo [" + ResourceLocation.CONF_PROPERTIES + ".properties] em getProperty()...");
 				FileInputStream fis = new FileInputStream( f );
-				//
 				prop.load( fis );
 				fis.close();
-				mapProperties.put(arquivoProperty, prop);
+				mapProperties.put(ResourceLocation.CONF_PROPERTIES, prop);
+				Log.info("Arquivo de propriedades: " + f.getAbsolutePath());
+				retorno = ((Properties)mapProperties.get(ResourceLocation.CONF_PROPERTIES)).getProperty(key);
 				
-				System.out.println(f.getAbsolutePath());
-				
-				retorno = ((Properties)mapProperties.get(arquivoProperty)).getProperty(key);
+
 			} catch(Exception e) {
-				Log.error("Ocorreu um erro na carga do arquivo: resources/" + arquivoProperty + ".properties");
+				Log.error("Ocorreu um erro na carga do arquivo: resources/" + ResourceLocation.CONF_PROPERTIES + ".properties");
 			}
 		}
-		//Log.info(this, "Buscando valor de " + key + " em " + arquivoProperty + ". Encontrado: " + retorno);
 		return retorno;
 	}
 	
@@ -100,41 +221,35 @@ public class ResourceUtil {
 	 * @return true se obter sucesso na gravação ou false se houverem erros.
 	 */
 	@SuppressWarnings("unchecked")
-	public boolean setProperty(String arquivoProperty, String key, String value) {
+	public boolean setProperty(String key, String value) {
 		boolean retorno = false;
-		//
-		f = new File(ResourceLocation.RESOURCES_PATH + arquivoProperty.concat(".properties"));
-		//
 		Properties props = null;
-		if (mapProperties.containsKey(arquivoProperty) ) {
-			props = (Properties) mapProperties.get(arquivoProperty);
+		if (mapProperties.containsKey(ResourceLocation.CONF_PROPERTIES) ) {
+			props = (Properties) mapProperties.get(ResourceLocation.CONF_PROPERTIES);
 		} else {
 			try {
-				//
 				FileInputStream fis = new FileInputStream(f);
-				//
 				props = new Properties();
-				Log.info("Processando a carga do arquivo [" + arquivoProperty + "] de propriedades em setProperty()...");
+				Log.info("Processando a carga do arquivo [" + ResourceLocation.CONF_PROPERTIES + "] de propriedades em setProperty()...");
 				props.load( fis );
-				mapProperties.put(arquivoProperty, props);
+				mapProperties.put(ResourceLocation.CONF_PROPERTIES, props);
 			} catch(Exception e) {
-				Log.info("Ocorreu um erro na carga do arquivo: resources/" + arquivoProperty + ".properties");
+				Log.info("Ocorreu um erro na carga do arquivo: resources/" + ResourceLocation.CONF_PROPERTIES + ".properties");
 			}
 		}
 		if (props != null) {
 			try {
 				FileOutputStream fos = new FileOutputStream( f );
-				//
 				props.setProperty(key, value);
 				props.store( fos, "Configuração registrada pelo componente " + getClass().getName() );
 				fos.flush();
 				fos.close();
 				retorno = true;
 			} catch (FileNotFoundException e) {
-				Log.error("Ocorreu um erro na abertura do arquivo [" + arquivoProperty + ".properties]!");
+				Log.error("Ocorreu um erro na abertura do arquivo [" + ResourceLocation.CONF_PROPERTIES + ".properties]!");
 				Log.error( e );
 			} catch (IOException e) {
-				Log.error("Ocorreu um erro de escrita no arquivo [" + arquivoProperty + ".properties]!");
+				Log.error("Ocorreu um erro de escrita no arquivo [" + ResourceLocation.CONF_PROPERTIES + ".properties]!");
 				Log.error( e );
 			}
 		}
