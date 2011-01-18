850b2ffa7cf94979f98a45a29ea2aba604702217
macielbombonato <macielbombonato@e08b796b-7f9d-4906-dee1-7c4ab3280acd>
2011-01-18 01:00:13 +0000

    Ajustes no mecanismo de controle de threads

--- src/org/mtec/sistemacarga/framework/ConcurrencyManager.java
+++ src/org/mtec/sistemacarga/framework/ConcurrencyManager.java
@@ -19,29 +19,36 @@ import org.mtec.sistemacarga.framework.util.Log;
 @SuppressWarnings("unchecked")
 final class ConcurrencyManager {
 	
+	private static int countThreads = 0;
+	
 	/**
 	 * Pool de threads da aplicação
 	 */
-	public static Thread[] t = new Thread[10];
+	public static Thread[] t = new Thread[5];
+	
 	/**
 	 * Pool de threads de monitoramento, normalmente utilizadas em situações onde
 	 * existam objetos com depêndias, objetos filhos.
 	 */
-	public static Thread[] monitor = new Thread[10];
+	public static Thread[] monitor = new Thread[5];
+	
 	/**
 	 * String que define o nome do objeto pai que deve ser executado antes que o
 	 * processo atual seja iniciado.
 	 */
 	public static String nomePai = "";
+	
 	/**
 	 * Nome do processo que será executado pelo processo.
 	 */
 	public static String nomeProcesso = "";
+	
 	/**
 	 * Nome da stored procedure que será executada no banco de dados para 
 	 * realizar a carga do banco de desenvolvimento
 	 */
 	public static String storedProcedure = "";
+	
 	/**
 	 * Lista de parâmetros da stored procedure
 	 */
@@ -73,54 +80,15 @@ final class ConcurrencyManager {
 		ConcurrencyManager.storedProcedure = storedProcedure;
 		ConcurrencyManager.parametros      = parametros;
 		//
+		realizaLimpezadeMemoria();
+		//
 		try {
 			DBCommandExecutor process = new DBCommandExecutor(storedProcedure, parametros); 
 			if ((nomePai == null) || (nomePai.equalsIgnoreCase(""))){
-				try {
-					for (int i = 0; i < t.length; i++) {
-						if (t[i] == null) {
-							t[i] = new Thread(process);
-							//
-							t[i].setName(nomeProcesso);
-							t[i].start();
-							break;
-						} else if (!threadAtiva(t[i])) {
-							t[i] = null;
-							t[i] = new Thread(process);
-							//
-							t[i].setName(nomeProcesso);
-							t[i].start();
-							break;
-						} else if (i == (t.length - 1)) {
-							i = -1;
-							Thread.sleep(10);
-						}
-					}
-				} catch (Exception e) {
-					Log.error("Erro ao tentar criar a thread " + nomeProcesso);
-					Log.error(e);
-				}
+				criaThread(nomeProcesso, process);
 			} else {
 				try {
-					for (int i = 0; i < monitor.length; i++) {
-						if (monitor[i] == null) {
-							monitor[i] = new Thread(new CargaFilhos());
-							//
-							monitor[i].setName("Monitor-" + nomePai);
-							monitor[i].start();
-							break;
-						} else if (!threadAtiva(t[i])) {
-							monitor[i] = null;
-							monitor[i] = new Thread(new CargaFilhos());
-							//
-							monitor[i].setName("Monitor-" + nomePai);
-							monitor[i].start();
-							break;
-						} else if (i == (monitor.length - 1)) {
-							i = -1;
-							Thread.sleep(10);
-						}
-					}
+					criaMonitordeThread(nomePai);
 				} catch (Exception e) {
 					Log.error("Erro ao tentar criar a thread de monitoramento " + "Monitor-" + nomePai);
 					Log.error(e);
@@ -129,6 +97,55 @@ final class ConcurrencyManager {
 		} catch (Exception e) {
 			Log.error("Erro ao tentar fazer a reflexão do processo: " + storedProcedure);
 			Log.error(e);
+		}
+	}
+
+	private void criaMonitordeThread(String nomePai) throws InterruptedException {
+		int i = 0;
+		while(true) {
+			if (i >= (monitor.length)) {
+				i = 0;
+				Thread.sleep(1);
+			} else if (monitor[i] == null || !threadAtiva(t[i])) {
+				monitor[i] = new Thread(new CargaFilhos());
+				monitor[i].setName("Monitor-" + nomePai);
+				monitor[i].start();
+				i++;
+				break;
+			} else {
+				i++;
+			}
+		}
+	}
+
+	private void criaThread(String nomeProcesso, DBCommandExecutor process) {
+		try {
+			int i = 0;
+			while(true) {
+				if (i >= (t.length)) {
+					i = 0;
+					Thread.sleep(1);
+				} else if (t[i] == null || !threadAtiva(t[i])) {
+					t[i] = new Thread(process);
+					t[i].setName(nomeProcesso);
+					t[i].start();
+					i++;
+					countThreads++;
+					break;
+				} else {
+					i++;
+				}
+			}
+		} catch (Exception e) {
+			Log.error("Erro ao tentar criar a thread " + nomeProcesso);
+			Log.error(e);
+		}
+	}
+
+	private void realizaLimpezadeMemoria() {
+		if (countThreads == 2500 || countThreads == 0) {
+			countThreads = 0;
+			System.gc();
 		}
 	}
 	
@@ -173,55 +190,9 @@ final class ConcurrencyManager {
 		try {
 			DBCommandExecutor process = new DBCommandExecutor(storedProcedure, parametros, padraoGenerico); 
 			if ((nomePai == null) || (nomePai.equalsIgnoreCase(""))){
-				try {
-					for (int i = 0; i < t.length; i++) {
-						if (t[i] == null) {
-							t[i] = new Thread(process);
-							//
-							t[i].setName(nomeProcesso);
-							t[i].start();
-							break;
-						} else if (!threadAtiva(t[i])) {
-							t[i] = null;
-							t[i] = new Thread(process);
-							//
-							t[i].setName(nomeProcesso);
-							t[i].start();
-							break;
-						} else if (i == (t.length - 1)) {
-							i = -1;
-							Thread.sleep(10);
-						}
-					}
-				} catch (Exception e) {
-					Log.error("Erro ao tentar criar a thread " + nomeProcesso);
-					Log.error(e);
-				}
+				criaThread(nomeProcesso, process);
 			} else {
-				try {
-					for (int i = 0; i < monitor.length; i++) {
-						if (monitor[i] == null) {
-							monitor[i] = new Thread(new CargaFilhos());
-							//
-							monitor[i].setName("Monitor-" + nomePai);
-							monitor[i].start();
-							break;
-						} else if (!threadAtiva(t[i])) {
-							monitor[i] = null;
-							monitor[i] = new Thread(new CargaFilhos());
-							//
-							monitor[i].setName("Monitor-" + nomePai);
-							monitor[i].start();
-							break;
-						} else if (i == (monitor.length - 1)) {
-							i = -1;
-							Thread.sleep(10);
-						}
-					}
-				} catch (Exception e) {
-					Log.error("Erro ao tentar criar a thread de monitoramento " + "Monitor-" + nomePai);
-					Log.error(e);
-				}
+				criaMonitordeThread(nomePai);
 			}
 		} catch (Exception e) {
 			Log.error("Erro ao tentar fazer a reflexão do processo: " + storedProcedure);
@@ -257,7 +228,7 @@ class CargaFilhos implements Runnable {
 				//
 				j = -1;
 				try {
-					Thread.sleep(10);
+					Thread.sleep(1);
 				} catch (Exception e) {
 					Log.error("Erro ao tentar executar o comando sleep da thread " + nomePai);
 					Log.error(e);
@@ -267,30 +238,27 @@ class CargaFilhos implements Runnable {
 		}
 		//
 		try {
-			for (int i = 0; i < ConcurrencyManager.t.length; i++) {
+			int i = 0;
+			while(true) {
 				try {
-					if (ConcurrencyManager.t[i] == null) {
+					if (i >= (ConcurrencyManager.t.length)) {
+						i = 0;
+						Thread.sleep(1);
+					} else if (ConcurrencyManager.t[i] == null || !ConcurrencyManager.threadAtiva(ConcurrencyManager.t[i])) {
 						ConcurrencyManager.t[i] = new Thread(new DBCommandExecutor(storedProcedure, parametros));
-						//
 						ConcurrencyManager.t[i].setName(nomeProcesso);
 						ConcurrencyManager.t[i].start();
+						i++;
 						break;
-					} else if (!ConcurrencyManager.threadAtiva(ConcurrencyManager.t[i])) {
-						ConcurrencyManager.t[i] = null;
-						ConcurrencyManager.t[i] = new Thread(new DBCommandExecutor(storedProcedure, parametros));
-						//
-						ConcurrencyManager.t[i].setName(nomeProcesso);
-						ConcurrencyManager.t[i].start();
-						break;
-					} else if (i == (ConcurrencyManager.t.length - 1)) {
-						i = -1;
-						Thread.sleep(10);
+					} else {
+						i++;
 					}
 				} catch (Exception e) {
+					System.gc();
 					ConcurrencyManager.t[i] = new Thread(new DBCommandExecutor(storedProcedure, parametros));
-					//
 					ConcurrencyManager.t[i].setName(nomeProcesso);
 					ConcurrencyManager.t[i].start();
+					i++;
 					break;
 				}
 			}
